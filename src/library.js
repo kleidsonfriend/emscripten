d5685ee5060aa1bdae263dfc206aa3967f3e1c4d
Sam Clegg <sbc@chromium.org>
2020-03-31 23:06:40 -0700

    Don't include fastcomp-specifc JS environ handlers under wasm
    Also ignore _get_environ in deps_info.json under the wasm backend
    since this symbol only exists under fastcomp

--- src/library.js
+++ src/library.js
@@ -760,6 +760,7 @@ LibraryManager.library = {
 #endif
   },
 
+#if !WASM_BACKEND
   // This object can be modified by the user during startup, which affects
   // the initial values of the environment accessible by getenv. (This works
   // in both fastcomp and upstream.
@@ -924,6 +925,7 @@ LibraryManager.library = {
     }
     return 0;
   },
+#endif
 
   getloadavg: function(loadavg, nelem) {
     // int getloadavg(double loadavg[], int nelem);
